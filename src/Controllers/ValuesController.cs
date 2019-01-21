03b3591d4d85439f1afec5b8ede7edfeeb8800e8
Daniel Donovan <danieldonovan@Daniels-MacBook-Pro.local>
2019-01-21 17:21:21 -0500

    Updated target

--- src/Controllers/ValuesController.cs
+++ src/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Realvision.LambdaTestApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2", "value3" };
+            return new string[] { "value1", "value2", "value3", "someNewValue", "value5" };
         }
 
         // GET api/values/5
